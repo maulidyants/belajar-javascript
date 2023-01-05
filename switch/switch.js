90627328fe8b3352fcaf3f1f4de547e2513647d9
maulidyants <maulidyants@gmail.com>
2023-01-05 21:52:26 +0800

    suit

--- switch/switch.js
+++ switch/switch.js
@@ -1,16 +1,22 @@
-var angka = prompt('masukkan angka');
+var item = prompt('masukkan nama makanan / minuman : \n (cth: nasi, daging, susu, hamburger, softdrink');
 
-switch( angka ) {
-    case 1 :
-        alert('anda memasukkan angka 1');
+switch( item ) {
+    case 'nasi' :
+        alert('makanan / minuman SEHAT');
         break;
-    case 2 :
-        alert('anda memasukkan angka 2');
+    case 'daging' :
+        alert('makanan / minuman SEHAT');
         break;
-    case 3 :
-        alert('anda memasukkan angka 3');
+    case 'susu' :
+        alert('makanan / minuman SEHAT');
+        break;
+    case 'hamburger' :
+        alert('makanan / minuman TIDAK SEHAT');
+        break;
+    case 'softdrink' :
+        alert('makanan / minuman TIDAK SEHAT');
         break;
     default :
-        alert('angka yang anda masukkan salah');
+        alert('anda memasukkan nama makanan / minuman yang salah');
         break;
 }
