c351847f1f4f95a72a6525ed048cdba05ec46d77
maulidyants <maulidyants@gmail.com>
2023-01-05 14:10:15 +0800

    IfElse

--- while/while.js
+++ while/while.js
@@ -1,7 +1,5 @@
-var jmlAngkot = 10;
-var noAngkot = 1;
-
-while( noAngkot <= jmlAngkot ) {
-    console.log('Angkot No. ' + noAngkot + ' beroperasi dengan baik');
-    noAngkot++;
+var ulang = true;
+while(ulang) {
+    console.log('hello world');
+    ulang = confirm('lagi');
 }
